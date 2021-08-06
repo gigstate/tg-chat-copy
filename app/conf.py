c304cfdbce4c8bc88dbb54863d20316c9811a642
gigstate <popkorn775@gmail.com>
2021-08-06 20:06:26 +0300

    multigroup support

--- app/conf.py
+++ app/conf.py
@@ -1,7 +1,10 @@
 import logging
 from environs import Env
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format=u"%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s",
+    level=logging.ERROR,
+)
 
 env = Env()
 env.read_env()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from pyrogram.handlers import MessageHandler
 from pyrogram import Client, filters, types
 from sys import exit
 from conf import env
@@ -6,28 +7,32 @@ from conf import env
 api_id = env.int("API_ID")
 api_hash = env.str("API_HASH")
 session = env.str("SESSION") or None
-src_chat = env.int("SOURCE") or None
+src_chats = env.list("SOURCE") or None
 dst_chat = env.int("DESTINATION") or None
 
 bot = Client(session, api_id, api_hash)
 media_group_ids = []
 
-@bot.on_message(filters.chat(src_chat) & filters.media_group)
 async def mg_handler(client, msg: types.Message) -> None:
     if msg.media_group_id not in media_group_ids:
         media_group_ids.append(msg.media_group_id)
-        await bot.copy_media_group(dst_chat, src_chat, msg.message_id)
+        await bot.copy_media_group(dst_chat, msg.chat.id, msg.message_id)
 
-@bot.on_message(filters.chat(src_chat) & filters.media)
 async def handler(client, msg: types.Message) -> None:
-    await bot.copy_message(dst_chat, src_chat, msg.message_id)
+    await bot.copy_message(dst_chat, msg.chat.id, msg.message_id)
 
 if __name__ == "__main__":
     if session is None:
         print("\nPlease enter session name in .env file")
         exit(1)
-    if (src_chat is None or dst_chat is None):
+    if (src_chats is None or dst_chat is None):
         print("\nPlease enter SOURCE and DESTINATION in .env file")
         exit(1)
-    print("Bot is running.")
+    print("Bot is starting.")
+    for i, src_chat in enumerate(src_chats, 1):
+        src_chat = int(src_chat)
+        print(src_chat, "handler added.")
+        bot.add_handler(MessageHandler(mg_handler, filters.chat(src_chat) & filters.media_group))
+        bot.add_handler(MessageHandler(handler, filters.chat(src_chat) & filters.media))
+    print("Handlers setup was finished successfully.")
     bot.run()
