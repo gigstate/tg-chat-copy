c7d8cdb64b240924797a32a60a25500f02a011e2
gigstate <popkorn775@gmail.com>
2021-08-06 21:51:43 +0300

    logging format

--- app/conf.py
+++ app/conf.py
@@ -1,9 +1,12 @@
 import logging
 from environs import Env
 
+date_strftime_format = "%H:%M:%S"
+
 logging.basicConfig(
-    format=u"%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s",
-    level=logging.ERROR,
+    format=u"%(filename)s:%(lineno)d [%(levelname)s] [%(asctime)s]  %(message)s",
+    datefmt=date_strftime_format,
+    level=logging.WARNING,
 )
 
 env = Env()
